256bf8cf8b4f39e35b2e4c83cf4c691e716eec11
fpirotti <fpirotti@gmail.com>
2020-08-12 14:08:29 +0200

    Fixed CMAKE

---
+++ include/helpers.h
@@ -0,0 +1,22 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+
+#include <laszip/laszip_api.h>
+
+
+
+static void dll_error(laszip_POINTER laszip)
+{
+  if (laszip)
+  {
+    laszip_CHAR* error;
+    if (laszip_get_error(laszip, &error))
+    {
+      fprintf(stderr,"DLL ERROR: getting error messages\n");
+    }
+    fprintf(stderr,"DLL ERROR MESSAGE: %s\n", error);
+  }
+}
+
+#endif // HELPERS_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,77 @@
 #include <iostream>
+#include <helpers.h>
+
+#if defined(_MSC_VER) && \
+    (_MSC_FULL_VER >= 150000000)
+#define LASCopyString _strdup
+#else
+#define LASCopyString strdup
+#endif
+
+#include <fstream>  // std::ofstream
+#include <algorithm> // std::copy
+#include <exception> // std::exception
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/common/common.h>
 
 using namespace std;
 
-int main()
+laszip_U8 version_major;
+laszip_U8 version_minor;
+laszip_U16 version_revision;
+laszip_U32 version_build;
+
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+
+    pcl::PCDReader reader;
+    pcl::PCDWriter writer;
+    std::stringstream ss;
+    //
+
+
+    if (laszip_load_dll())
+    {
+      fprintf(stderr,"DLL ERROR: loading LASzip DLL\n");
+    }
+
+    // get version of LASzip DLL
+
+    if (laszip_get_version(&version_major, &version_minor, &version_revision, &version_build))
+    {
+      fprintf(stderr,"DLL ERROR: getting LASzip DLL version number\n");
+    }
+    else
+    {
+      fprintf(stderr,"LASzip DLL v%d.%d r%d (build %d)\n", (int)version_major, (int)version_minor, (int)version_revision, (int)version_build);
+    }
+
+
+
+
+    try
+    {
+
+    }
+    catch (std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+
+
+    cout << "Hello World!   " <<  argc << endl;
+    if(argc>0) cout << " arg is " << argv[1] << endl;
     return 0;
+}
+
+void help(){
+
+}
+
+
+int las2pcl()
+{
+ return 0;
 }
